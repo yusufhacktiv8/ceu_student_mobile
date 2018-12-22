7bb917984d1eae96f301b80267c5f11e7f1b4cb9
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-12-23 01:26:56 +0700

    Dashboar bottom navigation bar

--- lib/dashboard/DashboardPage.dart
+++ lib/dashboard/DashboardPage.dart
@@ -1,28 +1,62 @@
 import 'package:flutter/material.dart';
 import './CourseSummaryChart.dart';
 import './CourseList.dart';
+import './../course/CoursePage.dart';
 
 class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text("Dashboard"),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CourseSummaryChart(),
-            Divider(),
-            CourseList(),
-          ],
+        appBar: new AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: new Text("Dashboard"),
         ),
-      ),
+        body: Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CourseSummaryChart(),
+              Divider(),
+              CourseList(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: new BottomNavigationBar(
+          currentIndex: 1,
+//          onTap: (int index) {
+//            setState(() {
+//              _screen = index;
+//            });
+//          },
+          items: [
+            new BottomNavigationBarItem(
+              icon: new Icon(Icons.home),
+              title: new Text('Home'),
+            ),
+            new BottomNavigationBarItem(
+              icon: new Icon(Icons.collections),
+              title: new Text('Courses'),
+            ),
+            new BottomNavigationBarItem(
+              icon: new Icon(Icons.account_circle),
+              title: new Text('Profile'),
+            ),
+          ],
+        )
     );
+//        body: new Center(
+//          // Center is a layout widget. It takes a single child and positions it
+//          // in the middle of the parent.
+//          child: new Column(
+//            mainAxisAlignment: MainAxisAlignment.center,
+//            children: <Widget>[
+//              CourseSummaryChart(),
+//              Divider(),
+//              CourseList(),
+//            ],
+//          ),
+//        ),
+//      );
   }
 }
