981767ba75e2189120fec537da1f5779c3e43ef1
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-12-29 23:33:45 +0700

    Schedules

--- lib/course/schedules.dart
+++ lib/course/schedules.dart
@@ -13,6 +13,85 @@ class _SchedulesState extends State<Schedules> {
         Card(
           child: ListTile(
             title: Text("Main"),
+            subtitle: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text("Plan: "),
+                    Text("01/01/2019 - 0106/2019"),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text("Actual: "),
+                    Text("01/01/2019 - 0106/2019"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text("Hospital 1"),
+            subtitle: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text("Plan: "),
+                    Text("01/01/2019 - 0106/2019"),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text("Actual: "),
+                    Text("01/01/2019 - 0106/2019"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text("Clinic"),
+            subtitle: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text("Plan: "),
+                    Text("01/01/2019 - 0106/2019"),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text("Actual: "),
+                    Text("01/01/2019 - 0106/2019"),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text("Hospital 2"),
+            subtitle: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Text("Plan: "),
+                    Text("01/01/2019 - 0106/2019"),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Text("Actual: "),
+                    Text("01/01/2019 - 0106/2019"),
+                  ],
+                ),
+              ],
+            ),
           ),
         )
       ],
