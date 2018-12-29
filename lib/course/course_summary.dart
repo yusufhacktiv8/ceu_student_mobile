b96e1d78e577a9d0da09cba51c148cfb08a942c4
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-12-29 17:29:39 +0700

    Course summary

--- lib/course/course_summary.dart
+++ lib/course/course_summary.dart
@@ -41,6 +41,76 @@ class _CourseSummaryState extends State<CourseSummary> {
                   child: Text("")
               )
             ],
+          ),
+          Padding(padding: EdgeInsets.all(5),),
+          Row(
+            children: <Widget>[
+              Text("3. Weekly Discussion"),
+              Text("42.0"),
+              Badge.before(
+                  value: "0.00 %",
+                  positionTop: 5,
+                  child: Text("")
+              )
+            ],
+          ),
+          Padding(padding: EdgeInsets.all(5),),
+          Row(
+            children: <Widget>[
+              Text("4. Case Test"),
+              Text("42.0"),
+              Badge.before(
+                  value: "0.00 %",
+                  positionTop: 5,
+                  child: Text("")
+              )
+            ],
+          ),
+          Padding(padding: EdgeInsets.all(5),),
+          Row(
+            children: <Widget>[
+              Text("5. Post Test"),
+              Text("42.0"),
+              Badge.before(
+                  value: "0.00 %",
+                  positionTop: 5,
+                  child: Text("")
+              )
+            ],
+          ),
+          Padding(padding: EdgeInsets.all(5),),
+          Row(
+            children: <Widget>[
+              Text("Total"),
+              Text("100.0"),
+              Badge.before(
+                  value: "0.00 %",
+                  positionTop: 5,
+                  child: Text("")
+              )
+            ],
+          ),
+          Padding(padding: EdgeInsets.all(5),),
+          Row(
+            children: <Widget>[
+              Text("Score"),
+              Text(""),
+              Text("A"),
+            ],
+          ),
+          Padding(padding: EdgeInsets.all(5),),
+          Row(
+            children: <Widget>[
+              Text("Status"),
+              Text(""),
+              Badge.before(
+                textStyle: TextStyle(fontSize: 2.0),
+                spacing: 5,
+                color: Colors.green,
+                value: " ",
+                child: Text("Completed"),
+              ),
+            ],
           )
         ],
       ),
